4b8fa51870ea4d52c5fc7c9405b2d65c41b8d154
Jamie <jamie.whitlam2109@gmail.com>
2024-12-31 15:13:01 +0000

    01/14

--- 01. Static pages/14. Custom Components/index.jsx
+++ 01. Static pages/14. Custom Components/index.jsx
@@ -1,16 +1,27 @@
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
 
 /**
  * Challenge (part 1):
  * Create a custom "Page" component
- * 
+ *
  * It should return an ordered list with the reasons why you're
  * excited to be learning React :)
- * 
+ *
  * Render the Page component.
  */
 
-root.render(
-    
-)
+root.render(<Page />);
+
+function Page() {
+  return (
+    <section>
+      <h1>Reasons why I'm excited to learn React, again:</h1>
+      <ol>
+        <li>I love learning new things</li>
+        <li>It's very popular</li>
+        <li>Something not on the Microsoft stack</li>
+      </ol>
+    </section>
+  );
+}
