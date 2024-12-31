19772edf9dd09847f9a089515c59a6e82ea3855b
Jamie <jamie.whitlam2109@gmail.com>
2024-12-31 14:10:35 +0000

    First react challenge.

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -3,3 +3,17 @@
  * This time, try rendering an unordered list with 2-3 list items inside
  * with why you're excited to be learning React.
  */
+
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    Reasons why I'm excited to learn React!
+    <ul>
+      <li>Very popular framework.</li>
+      <li>I love learning new skills!</li>
+      <li>Something not on the Microsoft stack!</li>
+    </ul>
+  </div>
+);
