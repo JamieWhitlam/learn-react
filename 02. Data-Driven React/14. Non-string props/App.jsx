f94d9c94212e9552e210d31cda28ff82275b2bce
Jamie <jamie.whitlam2109@gmail.com>
2025-01-01 14:43:19 +0000

    02/14

--- 02. Data-Driven React/14. Non-string props/App.jsx
+++ 02. Data-Driven React/14. Non-string props/App.jsx
@@ -15,22 +15,38 @@ export default function App() {
         <main>
             <Joke
                 punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+                upvotes={5}
+                downvotes={0}
+                comments={["This joke is great - Jamie", "I think this joke is really funny - Kayleigh"]}
+                isPun={false}
             />
             <Joke
                 setup="How did the hacker escape the police?"
                 punchline="He just ransomware!"
+                upvotes={5}
+                downvotes={0}
+                isPun={true}
             />
             <Joke
                 setup="Why don't pirates travel on mountain roads?"
                 punchline="Scurvy."
+                upvotes={5}
+                downvotes={0}
+                isPun={true}
             />
             <Joke
                 setup="Why do bees stay in the hive in the winter?"
                 punchline="Swarm."
+                upvotes={5}
+                downvotes={0}
+                isPun={true}
             />
             <Joke
                 setup="What's the best thing about Switzerland?"
                 punchline="I don't know, but the flag is a big plus!"
+                upvotes={5}
+                downvotes={0}
+                isPun={false}
             />
         </main>
     )
--- 02. Data-Driven React/14. Non-string props/Joke.jsx
+++ 02. Data-Driven React/14. Non-string props/Joke.jsx
@@ -1,9 +1,20 @@
 export default function Joke(props) {
-    return (
-        <>
-            {props.setup && <p className="setup">Setup: {props.setup}</p>}
-            <p className="punchline">Punchline: {props.punchline}</p>
-            <hr />
-        </>
-    )
+  const comments =
+    props.comments &&
+    props.comments.map(function (comment) {
+      return <li>{comment}</li>;
+    });
+
+  return (
+    <>
+      <p>{props.isPun ? "This is a pun" : "This is not a pun"}</p>
+      {props.setup && <p className="setup">Setup: {props.setup}</p>}
+      <p className="punchline">Punchline: {props.punchline}</p>
+      <p>Upvotes: {props.upvotes}</p>
+      <p>Downvotes: {props.downvotes}</p>
+      <p>Comments:</p>
+      <ul>{comments}</ul>
+      <hr />
+    </>
+  );
 }
