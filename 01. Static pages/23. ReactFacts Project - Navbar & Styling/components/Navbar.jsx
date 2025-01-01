8254e989cdc77b2adf28b81dcc3972510b4f4275
Jamie <jamie.whitlam2109@gmail.com>
2025-01-01 11:54:57 +0000

    01/23

--- 01. Static pages/23. ReactFacts Project - Navbar & Styling/components/Navbar.jsx
+++ 01. Static pages/23. ReactFacts Project - Navbar & Styling/components/Navbar.jsx
@@ -1,6 +1,6 @@
 /**
  * Challenge: complete the Navbar to match the Figma design
- * 
+ *
  * Hints:
  * - for semantic HTML purposes, the Navbar should render
  *   a <header> with a <nav> nested inside. The image and "ReactFacts"
@@ -9,8 +9,17 @@
  *   colors, sizes, font information, etc.
  */
 
+import logo from "/react-logo.png";
+
 export default function Navbar() {
-    return (
-        <h1>Navbar component here</h1>
-    )
+  return (
+    <header className="header">
+      <nav>
+        <a className="header-logo-wrapper" target="_self" href="#">
+          <img src={logo} alt="React logo" className="header-logo" />
+          <span className="header-logo-text">ReactFacts</span>
+        </a>
+      </nav>
+    </header>
+  );
 }
