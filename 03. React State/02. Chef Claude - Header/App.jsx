2a8aa8fe3784856e068fa559ea5eccfc9cf1de03
Jamie <jamie.whitlam2109@gmail.com>
2025-01-05 12:00:34 +0000

    03/02

--- 03. React State/02. Chef Claude - Header/App.jsx
+++ 03. React State/02. Chef Claude - Header/App.jsx
@@ -3,8 +3,10 @@
  * and render it here in the App component
  */
 
+import Header from "./components/Header"
+
 export default function App() {
   return (
-      <h1>Hello world!</h1>
+      <Header />
   )
 }
