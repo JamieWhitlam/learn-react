a9daeb44df81084d9e95cda82a01cce253d4c4d0
Jamie <jamie.whitlam2109@gmail.com>
2025-01-05 10:56:20 +0000

    02/17

--- 02. Data-Driven React/17. Review - array.map()/index.js
+++ 02. Data-Driven React/17. Review - array.map()/index.js
@@ -2,11 +2,16 @@
 Challenge 1:
 Given an array of numbers, return an array of each number, squared
 */
-const nums = [1, 2, 3, 4, 5]
+
+// const nums = [1, 2, 3, 4, 5]
 // -->       [1, 4, 9, 16, 25]
 // Your code here
 
+// const numsSquared = nums.map((num) => {
+//     return num * num;
+// });
 
+// console.log(numsSquared);
 
 
 /*
@@ -15,12 +20,14 @@ Given an array of strings, return an array where
 the first letter of each string is capitalized
 */
 
-const names = ["alice", "bob", "charlie", "danielle"]
+// const names = ["alice", "bob", "charlie", "danielle"]
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
 
-
-
+// const namesCapitalised = names.map((name) => {
+//     return name[0].toUpperCase() + name.substring(1, name.length);
+// })
+// console.log(namesCapitalised);
 
 /*
 Challenge 3:
@@ -31,8 +38,10 @@ E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
 return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 */
 
-const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+const pokemons = ["Bulbasaur", "Charmander", "Squirtle"]
 // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
 // Your code here
 
+const pokemonParagraphs = pokemons.map(pokemon => `<p>${pokemon}</p>`);
 
+console.log(pokemonParagraphs);
