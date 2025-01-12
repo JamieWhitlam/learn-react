446f48fb47df173e8c948ad578c8a192767dde16
Jamie <jamie.whitlam2109@gmail.com>
2025-01-12 13:17:32 +0000

    03/12

--- 03. React State/12. State practice/App.jsx
+++ 03. React State/12. State practice/App.jsx
@@ -6,13 +6,24 @@ export default function App() {
      * Create state to track our count value (initial value is 0)
      * Don't forget to replace the hard-coded "0" with your new state
      */
+    
+    const [count, setCount] = React.useState(0);
+
+    function handleDecreaseClick() {
+        setCount(count - 1);
+    }
+    
+    function handleIncreaseClick() {
+        setCount(count + 1);
+    }
+
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">0</h2>
-                <button className="plus" aria-label="Increase count">+</button>
+                <button onClick={handleDecreaseClick} className="minus" aria-label="Decrease count">–</button>
+                <h2 className="count">{count}</h2>
+                <button onClick={handleIncreaseClick} className="plus" aria-label="Increase count">+</button>
             </div>
         </main>
     )
